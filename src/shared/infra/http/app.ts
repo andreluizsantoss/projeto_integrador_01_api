7b78b9de0ce45f0f4a4d549408135d0e120aaee3
andreluizsantoss <andre.luiz.itape@gmail.com>
2025-03-24 20:27:58 -0300

    Implementação - Rotas no arquivo app

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,10 +4,12 @@ import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { ZodError } from 'zod'
+import { routes } from './routes'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(routes)
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
