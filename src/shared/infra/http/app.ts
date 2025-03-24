ebc0d6d2f3319ea91166f62686c0ecd7ec24ec62
andreluizsantoss <andre.luiz.itape@gmail.com>
2025-03-24 16:31:25 -0300

    Implementação - Arquivos app e server

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,34 @@
+import 'reflect-metadata'
+import { env } from '@shared/env'
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+import { ZodError } from 'zod'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ZodError) {
+      response.status(400).send({
+        message: 'Validation error',
+        issues: error.format(),
+      })
+    }
+
+    if (env.NODE_ENV !== 'production') {
+      console.log(error)
+    } else {
+      // TODO: Log para uma ferramenta externa
+    }
+
+    response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    })
+  },
+)
+
+export { app }
