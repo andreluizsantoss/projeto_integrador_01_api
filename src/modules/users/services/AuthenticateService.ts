d9048734e80bd141631801df953415774e624736
andreluizsantoss <andre.luiz.itape@gmail.com>
2025-04-15 21:02:19 -0300

    Refatoração - Module Users - Services - AuthenticateService

--- src/modules/users/services/AuthenticateService.ts
+++ src/modules/users/services/AuthenticateService.ts
@@ -1,6 +1,10 @@
+import { InvalidCredentialsError } from '@shared/errors/InvalidCredentialsError'
+import { IAuthenticateResponse } from '@users/domain/models/IAuthenticateResponse'
 import { IAuthenticateUser } from '@users/domain/models/IAuthenticateUser'
 import { IUsersRepository } from '@users/domain/repositories/IUsersRepository'
 import { inject, injectable } from 'tsyringe'
+import jwtConfig from '@config/auth'
+import { sign } from 'jsonwebtoken'
 
 @injectable()
 export class AuthenticateService {
@@ -9,5 +13,36 @@ export class AuthenticateService {
     private usersRepository: IUsersRepository,
   ) {}
 
-  async execute({ username, senha }: IAuthenticateUser): Promise<void> {}
+  async execute({
+    username,
+    senha,
+  }: IAuthenticateUser): Promise<IAuthenticateResponse> {
+    const user = await this.usersRepository.findByUsername(username)
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    if (senha != user.senha) {
+      throw new InvalidCredentialsError()
+    }
+
+    const access_token = sign({}, jwtConfig.jwt.secret, {
+      subject: user.id.toString(),
+      // expiresIn: jwtConfig.jwt.expiresIn,
+      issuer: 'sos-soro-pi-01',
+    })
+
+    const refresh_token = sign({}, jwtConfig.refreshToken.secret, {
+      // subject: user.id.toString(),
+      // expiresIn: jwtConfig.refreshToken.expiresIn,
+      // notBefore: jwtConfig.refreshToken.notBefore,
+      issuer: 'sos-soro-pi-01',
+    })
+
+    user.refresh_token = refresh_token
+
+    await this.usersRepository.autenthicateUser(user)
+
+    return { access_token, refresh_token }
+  }
 }
