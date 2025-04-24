9c89e9755642db1f26eae3dc09ca6472c31347ba
andreluizsantoss <andre.luiz.itape@gmail.com>
2025-04-24 19:20:22 -0300

    Implementação - Biblioteca Swagger-Ui

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,13 +3,16 @@ import { env } from '@shared/env'
 import '@shared/infra/http/container'
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
+import swaggerUi from 'swagger-ui-express'
 import cors from 'cors'
 import { routes } from './routes'
 import { ZodError } from 'zod'
+import swaggerFile from '../../../swagger.json'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(routes)
 
 app.use(
