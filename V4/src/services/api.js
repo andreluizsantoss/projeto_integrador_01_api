4ca191eea32d38acd5730cad334b94847cd75011
Leonardo da Cunha <118402904+LeonardoSCunha@users.noreply.github.com>
2025-11-03 22:37:35 -0300

    Update api.js

--- V4/src/services/api.js
+++ V4/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://projeto-integrador-01-api.onrender.com',
 });
 
 // Interceptor para adicionar token automaticamente
